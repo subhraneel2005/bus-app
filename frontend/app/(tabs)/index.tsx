3d40a8646cb8af50f74a0b20dd354cef05af16df
subhraneel2005 <digitalsubhraneel@gmail.com>
2025-08-12 21:50:35 +0530

    test commit

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -3,12 +3,8 @@ import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View className="bg-yellow-500 w-full h-screen flex-1 p-3">
-      <View className="flex-1 bg-white rounded-xl pt-6 items-center justify-center">
-        <Text className="text-4xl font-bold text-zinc-900 text-center">
-          Swastik 
-        </Text>
-      </View>
+    <View>
+      <Text>Home Screen</Text>
     </View>
   );
 }
