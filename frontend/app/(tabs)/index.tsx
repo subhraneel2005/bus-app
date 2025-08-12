90e13a753124a61f47e73ec009e19824da9b3afa
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-08-12 21:50:08 +0530

    8th commit

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ export default function HomeScreen() {
     <View className="bg-yellow-500 w-full h-screen flex-1 p-3">
       <View className="flex-1 bg-white rounded-xl pt-6 items-center justify-center">
         <Text className="text-4xl font-bold text-zinc-900 text-center">
-          Swastik <Text className="italic ml-1 text-green-600">Kalachuda</Text>
+          Swastik 
         </Text>
       </View>
     </View>
