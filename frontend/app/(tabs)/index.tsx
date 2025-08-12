25ad5a1d7d7922365b3cafd1045f52fc01e795aa
subhraneel2005 <digitalsubhraneel@gmail.com>
2025-08-12 21:40:02 +0530

    project init

---
+++ frontend/app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import "@/global.css";
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="bg-yellow-500 w-full h-screen flex-1 p-3">
+      <View className="flex-1 bg-white rounded-xl pt-6 items-center justify-center">
+        <Text className="text-4xl font-bold text-zinc-900 text-center">
+          Swastik <Text className="italic ml-1 text-green-600">Kalachuda</Text>
+        </Text>
+      </View>
+    </View>
+  );
+}
